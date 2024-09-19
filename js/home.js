a41a835ff66878b22461ee69df081a9a2636de26
forhad1518 <ahforhad20@gmail.com>
2024-09-19 08:07:41 +0600

    add money evenListener

---
+++ js/home.js
@@ -0,0 +1,8 @@
+document.getElementById('add-money-btn').addEventListener('click',function(event){
+    event.preventDefault()
+    
+    const inputAmount = document.getElementById('input-amount').value;
+    const inputPin = document.getElementById('input-pin').value;
+    
+    
+})
