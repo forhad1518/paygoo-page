2dd3da702bea4a0b7fc212fdcd74b7ccceb7b78e
forhad1518 <ahforhad20@gmail.com>
2024-09-19 08:21:27 +0600

    added add money evenListener

--- js/home.js
+++ js/home.js
@@ -4,5 +4,21 @@ document.getElementById('add-money-btn').addEventListener('click',function(event
     const inputAmount = document.getElementById('input-amount').value;
     const inputPin = document.getElementById('input-pin').value;
     
-    
+    if(inputPin === "1234"){
+        const amount = document.getElementById('recent-amount').innerText
+
+        const amountNumber = parseFloat(inputAmount);
+        const firstAmountNumber = parseFloat(amount);
+
+        const totalAmount = amountNumber + firstAmountNumber;
+        console.log(totalAmount)
+
+        document.getElementById('recent-amount').innerText = totalAmount;
+        alert('added '+ amountNumber + " taka")
+
+        
+    }
+    else{
+        alert("pin is incorrect! please try again.")
+    }
 })
