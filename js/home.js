6da898b678033fd03f06f528c14adf882bd83049
forhad1518 <ahforhad20@gmail.com>
2024-09-20 07:50:23 +0600

    cash Out event Listener added

--- js/home.js
+++ js/home.js
@@ -1,3 +1,4 @@
+// Add money eventListener
 document.getElementById('add-money-btn').addEventListener('click',function(event){
     event.preventDefault()
     
@@ -11,10 +12,34 @@ document.getElementById('add-money-btn').addEventListener('click',function(event
         const firstAmountNumber = parseFloat(amount);
 
         const totalAmount = amountNumber + firstAmountNumber;
-        console.log(totalAmount)
 
         document.getElementById('recent-amount').innerText = totalAmount;
         alert('added '+ amountNumber + " taka")
+
+        
+    }
+    else{
+        alert("pin is incorrect! please try again.")
+    }
+})
+
+// Cash Out evenListener
+document.getElementById('cash-out-btn').addEventListener('click',function(event){
+    event.preventDefault()
+
+    const inputAmount = document.getElementById('input-cashOut-amount').value;
+    const inputPin = document.getElementById('input-cashOut-pin').value;
+    
+    if(inputPin === "1234"){
+        const amount = document.getElementById('recent-amount').innerText
+
+        const amountNumber = parseFloat(inputAmount);
+        const firstAmountNumber = parseFloat(amount);
+
+        const totalAmount = firstAmountNumber - amountNumber;
+
+        document.getElementById('recent-amount').innerText = totalAmount;
+        alert('Cash Out '+ amountNumber + " taka")
 
         
     }
